e9aaffd078ea84ee310c31ba30969f09a898b43d
Ari Sobel <arieh.sobel@gmail.com>
2020-04-21 14:19:51 -0300

    criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,14 +1,15 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=10):
+    def __init__(self, *filhos, nome=None, idade=10):
         self.nome = nome
         self.idade = idade
+        self.filhos = list(filhos)
     def cumprimentar(self):
         return f"olá {self.nome} id({id(self)})"
         
 if __name__ == "__main__":
-    p = Pessoa('João')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    p.nome= "Ari"
-    print(p.cumprimentar())
-    print(p.idade)
+    joao = Pessoa(nome='João')
+    pedro = Pessoa(joao,nome='Pedro')
+    print(Pessoa.cumprimentar(pedro))
+    print(id(joao))
+    for filho in pedro.filhos:
+        print(filho.nome)
